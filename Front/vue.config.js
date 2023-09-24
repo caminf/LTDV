0d9906f8b4f0eeae94ee83529c2efe33d479f35f
Carlos Esteban <48688160+CarlosEsteban-afk@users.noreply.github.com>
2023-09-24 13:20:02 -0300

    Delete Front directory

--- Front/vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave:false
-})
-//lintOnSave, no se que hizo, pero saco el mensaje molesto de
-//Component name "Home" should always be multi-word
