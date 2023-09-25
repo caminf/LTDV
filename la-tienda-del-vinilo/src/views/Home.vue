8ad20ed3738f3d0e05bcca02aa2e4a52678a9e1e
Carlos <c.pena17@ufromail.cl>
2023-09-25 10:20:34 -0300

    add cards to home view

--- la-tienda-del-vinilo/src/views/Home.vue
+++ la-tienda-del-vinilo/src/views/Home.vue
@@ -1,15 +1,28 @@
 <template>
     <main class="home-page">
-        <Searchbar></Searchbar>
-        <h1>Home</h1>
-        <p>this is a home page</p>
-
+        <Searchbar />
+        <h1>Vinilos</h1>
+        <v-sheet class="d-flex flex-wrap ">
+            <Card v-for="datas in data" :name="datas.name" :description="datas.description" :rating="datas.rating"
+                :price="datas.price"></Card>
+        </v-sheet>
 
     </main>
 </template>
 
 <script setup>
 import Searchbar from '../components/Searchbar.vue';
+import Card from '../components/Card.vue';
+import data from '../data/data.js';
+
+
+
 </script>
 
-<style></style>
+<style lang="scss">
+.item {
+    display: flex;
+    border: 1px solid blue;
+    align-content: flex-start;
+}
+</style>
